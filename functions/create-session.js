3c28f432f10729fa8afdbb3c9e661695786b74eb
Jason Lengstorf <jason@lengstorf.com>
2021-04-27 11:34:07 -0700

    fix: use the serverless utilities

--- functions/create-session.js
+++ functions/create-session.js
@@ -1,5 +1,6 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const formatLineItems = require("use-shopping-cart/utilities").formatLineItems;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const formatLineItems = require('use-shopping-cart/utilities/serverless')
+  .formatLineItems;
 
 exports.handler = async (event) => {
   const cartDetails = JSON.parse(event.body);
@@ -7,12 +8,12 @@ exports.handler = async (event) => {
   const line_items = formatLineItems(cartDetails);
 
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    billing_address_collection: "auto",
+    payment_method_types: ['card'],
+    billing_address_collection: 'auto',
     shipping_address_collection: {
-      allowed_countries: ["US", "CA"],
+      allowed_countries: ['US', 'CA'],
     },
-    mode: "payment",
+    mode: 'payment',
     success_url: `${process.env.URL}/success.html`,
     cancel_url: process.env.URL,
     line_items,
